7e31a276e4c6363f9fa4c8c5953fe1c3be3ee3c3
opeth211 <joe_zipeto@hotmail.com>
2016-09-30 23:05:02 -0400

    Add files via upload

---
+++ Assignment1/Assets/Scripts/GrassController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 0;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform>();
+		_currentPosition = _transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = transform.position;
+
+		_currentPosition -= new Vector2 (speed,0);
+		_transform.position = _currentPosition;
+
+		if (_currentPosition.x < -10.0f) {
+			Debug.Log (_currentPosition.x);
+			Reset ();
+		}
+	}
+
+	private void Reset(){
+		
+		_currentPosition = new Vector2 (10.0f, 0);
+		_transform.position = _currentPosition;
+	}
+}
+
---
+++ Assignment1/Assets/Scripts/RobotController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 0;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.transform;
+		_currentPosition = _transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float input = Input.GetAxis ("Vertical");
+
+		if (input > 0)
+			_currentPosition += new Vector2 (0, speed);
+
+		if (input < 0)
+			_currentPosition -= new Vector2 (0, speed);
+
+		checkBounds ();
+		_transform.position = _currentPosition;
+
+	}
+
+	private void checkBounds(){
+
+		if (_currentPosition.y < -3.5f) {
+			_currentPosition.y = -3.5f;
+		}
+		if (_currentPosition.y > 3.5f) {
+			_currentPosition.y = 3.5f;
+		}
+	}
+	
+}
